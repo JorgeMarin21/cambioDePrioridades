6bbdfa9d084f44ca520e7ac1519e4594195c7a65
Jorge Marin <jorge_marin@live.com>
2020-09-29 22:52:51 -0500

    actualizacion en cambios de procesos

--- src/app/services/process-info.service.ts
+++ src/app/services/process-info.service.ts
@@ -6,6 +6,8 @@ import { Injectable } from '@angular/core';
 export class ProcessInfoService {
   process: any;
   processChanged: any;
+  processRecurrent = [];
+  lastValueList = [];
   numberOne: number;
   numberTwo: number;
   showProcesses = false;
@@ -19,37 +21,55 @@ export class ProcessInfoService {
   }
   addProcess(proc) {
     this.process = proc;
+    if (this.processRecurrent.length !== 0) {
+      this.processRecurrent = [];
+    }
   }
   getProcess() {
     return this.process;
+  }
+  getRecurrentProcess() {
+    return this.processRecurrent;
   }
   getProccessChanged() {
     const body = {processChanged: this.processChanged, num1: this.numberOne, num2: this.numberTwo};
     return body;
   }
+  getLastValueList() {
+    return this.lastValueList;
+  }
   isFilled() {
     return this.process !== undefined;
   }
   changeListOrder() {
+    const listBackUp = [];
+    if (this.processRecurrent.length === 0) {
+      this.processRecurrent.push(...this.process.processList);
+    }
     let randomNum1 = 0;
     let randomNum2 = 0;
-    console.log('array #1', this.process.processList);
-    const listBackUp = [];
+    console.log('array #1', this.processRecurrent);
     do {
-      randomNum1 = Math.floor(Math.random() * this.process.processList.length);
-      randomNum2 = Math.floor(Math.random() * this.process.processList.length);
+      randomNum1 = Math.floor(Math.random() * this.processRecurrent.length);
+      randomNum2 = Math.floor(Math.random() * this.processRecurrent.length);
     } while (randomNum1 <= randomNum2);
-    for (let index = 0; index < this.process.processList.length; index++) {
+
+    for (let index = 0; index < this.processRecurrent.length; index++) {
       if ( index < randomNum2 || index > randomNum1) {
-        listBackUp.push(this.process.processList[index]);
+        listBackUp.push(this.processRecurrent[index]);
       } else if (index === randomNum2) {
-        listBackUp.push(this.process.processList[randomNum1]);
+        listBackUp.push(this.processRecurrent[randomNum1]);
       } else {
-        listBackUp.push(this.process.processList[index - 1]);
+        listBackUp.push(this.processRecurrent[index - 1]);
       }
     }
     console.log('array #2', listBackUp);
     this.processChanged = listBackUp;
+    this.lastValueList = [];
+    this.lastValueList.push(...this.processRecurrent);
+    console.log('array #3', this.lastValueList);
+    this.processRecurrent = [];
+    this.processRecurrent.push(...listBackUp);
     this.numberOne = randomNum1;
     this.numberTwo = randomNum2;
     return {listBackUp, randomNum1, randomNum2};
